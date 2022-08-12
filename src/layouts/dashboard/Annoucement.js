35cf6b524844952365db0b7dcc5f283eb27d4ced
kesaviwebsolutions <89696473+mynkbhati22@users.noreply.github.com>
2022-08-12 19:30:11 +0530

    update-betting-app

--- src/layouts/dashboard/Annoucement.js
+++ src/layouts/dashboard/Annoucement.js
@@ -1,13 +1,64 @@
+/* eslint-disable no-var */
+/* eslint-disable no-undef */
 /* eslint-disable no-plusplus */
 import React, { useState, useEffect } from 'react';
 import { Box, Container, ImageList, ImageListItem, Link, Typography } from '@mui/material';
+import $ from 'jquery';
 import Client from '../../Client';
 import fifa from '../../components/images/fifa inu.png';
 import metfx from '../../components/images/metfx (2).png';
 import CustodyW from '../../components/images/Custody.png';
 import smartath from '../../components/images/smartath.png';
 
+//   $(document).ready(() => {
+//    function handleMarquee() {
+//     const marquee = document.querySelectorAll('.marquee-js');
+//     var windowsize = $(window).width();
+//     const mobileSpeed = 2;
+//     const desktopSpeed = 1;
+//     let speed;
 
+//     if (windowsize <= 768) {
+//       speed = mobileSpeed;
+//     } else {
+//       speed = desktopSpeed;
+//     }
+
+//     // let speed = s;
+
+//     // let timer;
+//     marquee.forEach((el) => {
+//       const container = el.querySelector('.marquee-inner');
+//       const content = el.querySelector('.marquee-inner > *');
+//       // Get total width
+//       const elWidth = content.offsetWidth;
+
+//       // Duplicate content
+//       let clone = content.cloneNode(true);
+//       container.appendChild(clone);
+//       clone = content.cloneNode(true);
+//       container.appendChild(clone);
+
+//       let progress = 1;
+//       function loop() {
+//         progress -= speed;
+//         if (progress <= elWidth * -1) {
+//           progress = 0;
+//         }
+//         container.style.transform = `translateX(${progress}px)`;
+//         container.style.transform += `skewX(${speed * 0.4}deg)`;
+//         window.requestAnimationFrame(loop);
+//       }
+//       loop();
+//     });
+//   }
+
+//   $(window).on('resize', () => {
+//     handleMarquee();
+//   });
+
+//   handleMarquee();
+// });
 
 export default function Annoucement() {
   const [annoucement, setAnnouncement] = useState();
@@ -56,6 +107,19 @@ export default function Annoucement() {
             ))}
         </Box>
       </Box>
+      {/* <section className="marquee-js">
+        <div className="marquee-inner">
+          <div className="marquee-content">
+            <div className="marquee-text">WEDDINGS</div>
+            <div className="marquee-text">CORPORATE EVENTS</div>
+            <div className="marquee-text">STREET MARKETS</div>
+            <div className="marquee-text">BIRTHDAY PARTIES</div>
+            <div className="marquee-text">GALAS</div>
+            <div className="marquee-text">FUNDRAISERS</div>
+            <div className="marquee-text">PICNICS</div>
+          </div>
+        </div>
+      </section> */}
     </Container>
   );
 }
