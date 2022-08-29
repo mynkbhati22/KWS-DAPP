81c89de5a61ad724b42b6853474117bc26fc8265
mayank6253 <89696473+mynkbhati22@users.noreply.github.com>
2022-08-29 18:18:34 +0530

    updated-upload-image

--- src/pages/UploadImages.js
+++ src/pages/UploadImages.js
@@ -1,39 +1,27 @@
-import { Box, ImageList, ImageListItem, Typography } from '@mui/material';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Box, Button, ImageList, ImageListItem, Typography } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import CloseIcon from '@mui/icons-material/Close';
 import { Container } from '@mui/system';
 import React from 'react';
 import avatar from '../components/images/avatar_1.jpg';
+import './upload.css';
 
 export default function UploadImages() {
   return (
     <>
       <Container maxWidth="lg">
-        <Box sx={{ marginTop: '70px' }}>
-          <Box className="wrapper">
-            <ImageList className="d-block m-auto w-50">
-              <ImageListItem>
-                <img src={avatar} alt="" />
-              </ImageListItem>
-            </ImageList>
-            <Box className="cloud-icon">
-              <CloudUploadIcon className="d-block m-auto" />
-            </Box>
-            <Box className="text-center">
-              <Typography paragraph>No File Choosen!</Typography>
-            </Box>
-            <Box className="cancel-icon">
-              <CloseIcon className="d-block m-auto"/>
-            </Box>
-            <Box className="text-center">
-              <Typography paragraph>File Name Here</Typography>
-            </Box>
-            <Box className='fileChoosen'>
-              <input id="default-btn" type="file"/>
-            </Box>
-            
-          </Box>
-        </Box>
+        <div className="container">
+          <div className="avatar-upload">
+            <div className="avatar-edit">
+              <input type="file" id="imageUpload" accept=".png, .jpg, .jpeg" />
+              <label hmtlFor="imageUpload" />
+            </div>
+            <div className="avatar-preview">
+              <div id="imagePreview" style={{ backgroundImage: "url('');" }} />
+            </div>
+          </div>
+        </div>
       </Container>
     </>
   );
