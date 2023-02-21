2127f21b1c8a014342f1828a56e4eabd5f86dbde
mynkbhati22 <mayankbhati1911@gmail.com>
2023-02-21 18:54:11 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 
 // ----------------------------------------------------------------------
-window.URL = 'https://jjczsr.deta.dev';
-// window.URL = 'https://9f3a-2402-4cc0-2502-39e-5d4a-f759-71b6-9a79.in.ngrok.io';
+// window.URL = 'https://jjczsr.deta.dev';
+window.URL = 'http://localhost:9000';
 
 export default function App() {
   return (
